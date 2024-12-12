bb427a691be08014426fc702f51812a61361e1f4
callalilyleaf <che23035@byui.edu>
2024-12-11 22:23:58 -0700

    finishe the Fullscreen Overlay Nav method on home page

--- byui-tw-student/temp.js
+++ byui-tw-student/temp.js
@@ -16,3 +16,11 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.width = "0%";
+}
